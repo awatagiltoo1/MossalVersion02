c0f245b55e6e949bd9e3e97b72d3772e234e4e65
awatagiltoo1 <awagitagiltoo@gmail.com>
2025-02-01 23:48:48 +0000

    Ajout collaborateur et configiration CICD

--- tests/ExecutionTest.spec.ts
+++ tests/ExecutionTest.spec.ts
@@ -10,12 +10,10 @@ const birthdayDate = faker.date.birthdate({ min: 18, max: 65, mode: 'age' });
 const birthday = birthdayDate.toISOString().split('T')[0];
 const profilAdresse = faker.location.streetAddress();
 const pourcentage = faker.number.int({ min: 1, max: 100 });
-//const phoneNumber = faker.phone.number('76#######');
-//verifier si a nvelle branche a été créé
+
 test.describe('TestSuite Collaborateurs', () => {
 test('Ajout collaborateur', async ({ collaboratorPage, page}) => {
-    //addCollaborator(firstname: string, lastname: string, email: string, phone: string,  birthdate: string, matricule: string, salary: string) 
-
+    
     await collaboratorPage.addCollaborator(prenom, nom, email, '77' +telephone.toString(), birthday, identifiant.toString(), '2000000');
 });
 });
