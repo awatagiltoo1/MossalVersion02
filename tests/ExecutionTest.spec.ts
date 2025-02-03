1531f4ebe41c51c32ac214aa2e6acfa731345891
awatagiltoo1 <awagitagiltoo@gmail.com>
2025-02-03 12:05:50 +0000

    Vérifier présence différents types de demande

--- tests/ExecutionTest.spec.ts
+++ tests/ExecutionTest.spec.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 import {test, expect } from '../utils/myTests';
-//import { CollaboratorPage } from '../pages/CollaboratorPage';
+
 const email = faker.internet.email();
 const prenom = faker.person.firstName();
 const nom = faker.person.lastName();
@@ -8,19 +8,28 @@ const identifiant = faker.number.int({ min: 1000, max: 9999 })
 const telephone = faker.number.int({min: 1000000, max:9999999})
 const birthdayDate = faker.date.birthdate({ min: 18, max: 65, mode: 'age' });
 const birthday = birthdayDate.toISOString().split('T')[0];
-//const profilAdresse = faker.location.streetAddress();
-//const pourcentage = faker.number.int({ min: 1, max: 100 });
 
-test.describe('TestSuite Collaborateurs', () => {
+
+/*test.describe('TestSuite Collaborateurs', () => {
 test('Ajout collaborateur', async ({ collaboratorPage, page}) => {
     
     await collaboratorPage.addCollaborator(prenom, nom, email, '+22177' +telephone.toString(), birthday, identifiant.toString(), '2000000');
 });
+});*/
+
+test('Ajout collaborateur', async ({ collaboratorPage, page}) => {
+    
+    await collaboratorPage.addCollaborator(prenom, nom, email, '+22177' +telephone.toString(), birthday, identifiant.toString(), '2000000');
 });
 
-test.describe('TestSuite Demande', () => {
+/*test.describe('TestSuite Demande', () => {
     test('Visualiser les différets types demandes', async ({ visibiliteTypesDemandesPage, page}) => {
         
         await visibiliteTypesDemandesPage.VisibiliteTypesDemandes();
     });
+    });*/
+
+    test('Visualiser les différets types demandes', async ({ visibiliteTypesDemandesPage, page}) => {
+        
+        await visibiliteTypesDemandesPage.VisibiliteTypesDemandes();
     });
